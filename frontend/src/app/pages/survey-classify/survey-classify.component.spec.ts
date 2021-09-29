54e3ff1cd8dfe459e5b9704931df5803d0489e75
wuyang <349413799@qq.com>
2021-09-30 00:37:42 +0800

    update ng/cli

--- frontend/src/app/pages/survey-classify/survey-classify.component.spec.ts
+++ frontend/src/app/pages/survey-classify/survey-classify.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { SurveyClassifyComponent } from './survey-classify.component';
 
@@ -6,7 +6,7 @@ describe('SurveyClassifyComponent', () => {
   let component: SurveyClassifyComponent;
   let fixture: ComponentFixture<SurveyClassifyComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SurveyClassifyComponent ]
     })
