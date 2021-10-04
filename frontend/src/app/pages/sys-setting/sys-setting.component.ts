1bf38faca688333adc8c0c2013661fefe06ab3cf
wuyang <349413799@qq.com>
2021-10-04 12:18:14 +0800

    feat: 成员菜单设置

--- frontend/src/app/pages/sys-setting/sys-setting.component.ts
+++ frontend/src/app/pages/sys-setting/sys-setting.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { MemberService } from '../../core/member.service';
 
 @Component({
   selector: 'app-sys-setting',
   templateUrl: './sys-setting.component.html',
-  styleUrls: ['./sys-setting.component.less']
+  styleUrls: ['./sys-setting.component.less'],
 })
 export class SysSettingComponent implements OnInit {
+  constructor(public memberService: MemberService) {}
 
-  constructor() { }
+  memberId: string;
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
